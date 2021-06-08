bca63fdec48dc25af1fd5fbe37ebc7c5ce61f586
Volodymyr Shturma <shturma.v@gmail.com>
2021-06-08 17:56:24 +0300

    Add vcd vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,31 @@ variable "vcd_template_name" {
   description = "Template name for VMs"
   default = "CentOS7tmpl"
 }
+
+##########################
+# vCloud connection info #
+##########################
+
+variable "vcd_user" {
+  description = "vCloud username"
+}
+
+variable "vcd_pass" {
+  description = "vCloud password"
+}
+
+variable "vcd_org" {
+  description = "vCloud Organization"
+}
+
+variable "vcd_vdc" {
+  description = "vCloud Virtual Datacenter"
+}
+
+variable "vcd_url" {
+  description = "vCloud URL"
+}
+
+variable "allow_unverified_ssl" {
+  description = "Allow connection to self-signed VCD SSL certificates"
+}
