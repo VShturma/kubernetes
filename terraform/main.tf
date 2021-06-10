dd87c78187c5b1d677f50e82c3f361593ea23c97
Volodymyr Shturma <shturma.v@gmail.com>
2021-06-10 13:36:32 +0300

    Add worker nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,7 @@ resource "vcd_vapp_vm" "master" {
   template_name = var.vcd_template_name
   memory = 1024
   cpus = 2
+
   network {
     type = "org"
     name = "10.77.0.1/24"
@@ -23,6 +24,30 @@ resource "vcd_vapp_vm" "master" {
     ip = "10.77.0.5${count.index + 1}"
     connected = true
   }
+
+  customization {
+    enabled = true
+  }
+}
+
+resource "vcd_vapp_vm" "worker" {
+  count = 2
+  vapp_name = vcd_vapp.kube.name
+  name = "worker${count.index + 1}"
+  computer_name = "worker${count.index + 1}.localhost"
+  catalog_name = var.vcd_catalog_name
+  template_name = var.vcd_template_name
+  memory = 2048
+  cpus = 2
+
+  network {
+    type = "org"
+    name = "10.77.0.1/24"
+    ip_allocation_mode = "MANUAL"
+    ip = "10.77.0.5${count.index + 2}"
+    connected = true
+  }
+
   customization {
     enabled = true
   }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "master" {
   value = vcd_vapp_vm.master[*].network[0].ip
 }
+
+output "worker" {
+  value = vcd_vapp_vm.worker[*].network[0].ip
+}
+
